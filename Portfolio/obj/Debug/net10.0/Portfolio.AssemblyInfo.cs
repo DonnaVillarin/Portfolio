ac171a6c3d79293408bfe7c69f56a4c1d590215e
DonnaVillarin <donnavillarin18@gmail.com>
2026-04-03 17:40:49 +0800

    updated code

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a52ba6c9eda9038d83b675817bb30b980592fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
